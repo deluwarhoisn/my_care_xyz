143dcd797b4d9b92337bba8e9d63168cde346a4d
deluwarhoisn <babuhossen301@gmail.com>
2025-12-24 22:36:23 +0600

    space

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -7,6 +7,7 @@ export default function Booking() {
   return (
     <div className="max-w-3xl mx-auto mt-16 p-6 bg-white shadow rounded-lg text-center">
       <h1 className="text-3xl font-bold mb-4">Booking Page</h1>
+      
       <p>Welcome! Here you can book your selected service.</p>
     </div>
   );
